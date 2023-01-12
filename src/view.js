0446c5892cc008380f8952311c96ae59e3f93e88
MikRyam <mikhailryamov@gmail.com>
2023-01-12 10:01:15 +0300

    modify view.js

--- src/view.js
+++ src/view.js
@@ -1,6 +1,5 @@
 const renderError = ({ feedbackEl }, error, i18nextInstance) => {
   if (error) {
-    console.log('errorRender', error);
     feedbackEl.classList.add('text-danger');
     feedbackEl.textContent = i18nextInstance.t(error);
   }
@@ -9,12 +8,10 @@ const renderError = ({ feedbackEl }, error, i18nextInstance) => {
 const renderState = (elements, state) => {
   switch (state) {
     case 'valid':
-      console.log(state);
       elements.input.classList.remove('is-invalid');
       elements.feedbackEl.innerHTML = '';
       break;
     case 'ready':
-      console.log(state);
       elements.form.reset();
       elements.input.focus();
       break;
@@ -22,43 +19,36 @@ const renderState = (elements, state) => {
       elements.input.classList.add('is-invalid');
       break;
     default:
-      // https://ru.hexlet.io/blog/posts/sovershennyy-kod-defolty-v-svitchah
       throw new Error(`Unknown process state: ${state}`);
   }
 };
 
 const renderFetchingDataFeedback = ({ feedbackEl }, value, i18nextInstance) => {
-  switch (value) { // waiting, loading, success, failed
+  switch (value) {
     case 'waiting':
       feedbackEl.classList.remove('text-success');
       feedbackEl.classList.remove('text-warning');
       feedbackEl.classList.add('text-danger');
       feedbackEl.innerHTML = '';
-      console.log(value);
       break;
     case 'loading':
       feedbackEl.classList.remove('text-success');
       feedbackEl.classList.remove('text-danger');
       feedbackEl.classList.add('text-warning');
       feedbackEl.textContent = i18nextInstance.t('form.feedback.loading');
-      // feedbackEl.innerHTML = '';
-      console.log(value);
       break;
     case 'failed':
-      console.log(value);
       feedbackEl.classList.remove('text-success');
       feedbackEl.classList.remove('text-warning');
       feedbackEl.classList.add('text-danger');
       break;
     case 'success':
-      console.log(value);
       feedbackEl.classList.remove('text-danger');
       feedbackEl.classList.remove('text-warning');
       feedbackEl.classList.add('text-success');
       feedbackEl.textContent = i18nextInstance.t('form.feedback.success');
       break;
     default:
-      // https://ru.hexlet.io/blog/posts/sovershennyy-kod-defolty-v-svitchah
       throw new Error(`Unknown process state: ${value}`);
   }
 };
@@ -69,7 +59,6 @@ const renderFeeds = ({ feedsContainer }, value, i18nextInstance) => {
   cardBorder.classList.add('card', 'border-0');
   const cardBody = document.createElement('div');
   cardBody.classList.add('card-body');
-  // cardBorder.prepend(cardBody);
   const feedsTitle = document.createElement('h2');
   feedsTitle.classList.add('card-title', 'h4');
   feedsTitle.textContent = i18nextInstance.t('feeds.title');
@@ -112,8 +101,8 @@ const renderPosts = ({ postsContainer }, value, i18nextInstance) => {
     li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
     const a = document.createElement('a');
     a.setAttribute('href', post.link);
-    post.viewed ? a.classList.add('fw-normal', 'link-secondary') : a.classList.add('fw-bold');
-    // a.classList.add(post.viewed ? ['fw-normal', 'link-secondary'] : 'fw-bold');
+    // post.viewed ? a.classList.add('fw-normal', 'link-secondary') : a.classList.add('fw-bold');
+    a.classList.add(post.viewed ? ['fw-normal', 'link-secondary'] : 'fw-bold');
     a.setAttribute('data-id', post.id);
     a.setAttribute('target', '_blank');
     a.setAttribute('rel', 'noopener noreferrer');
